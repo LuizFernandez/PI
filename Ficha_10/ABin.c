8d7365129b895123228284f8f01b6e7dc501bd78
LuizFernandez <luiz.mtfernandez13@gmail.com>
2021-06-09 23:12:15 +0100

    Ficha 10, Falta as funçoes espinha

---
+++ Ficha_10/ABin.c
@@ -0,0 +1,147 @@
+
+#include "ABin.h"
+
+ABin newABin (int r, ABin e, ABin d) {
+   ABin a = malloc (sizeof(struct nodo));
+   if (a!=NULL) {
+      a->valor = r; a->esq = e; a->dir = d;
+   }
+   return a;
+}
+
+ABin RandArvFromArray (int v[], int N) {
+   ABin a = NULL;
+    int m;
+    if (N > 0){
+    	m = rand() % N;
+    	a = newABin (v[m], RandArvFromArray (v,m), RandArvFromArray (v+m+1,N-m-1));
+    }
+    return a;	
+}
+
+void dumpABin(ABin a, int N){
+    if(a){
+        dumpABin(a->esq, N);
+        printf("%d ", a->valor);
+        dumpABin(a->dir, N);
+    }
+}
+
+void freeABin(ABin a){
+    if(a){
+        freeABin(a->dir);
+        freeABin(a->esq);
+        free(a);
+    }
+}
+
+// Questão 1
+ABin removeMenor (ABin *a){
+
+    ABin aux, r = NULL;
+    if(*a){
+        while((*a)->esq)
+            a = &((*a)->esq);
+        aux = *a;
+        r = *a;
+        *a = (*a)->dir;    
+    }
+
+    return r;
+
+}
+
+void removeRaiz (ABin *a){
+
+    ABin menor, aux;
+    if(*a){
+        if((*a)->dir){
+            menor = removeMenor(&((*a)->dir));
+            aux = (*a);
+            menor->esq = (*a)->esq;
+            menor->dir = (*a)->dir;
+            (*a) = menor;
+        } else {
+            aux = (*a);
+            (*a) = (*a)->dir;
+        }
+
+        free(aux);
+    }
+}
+
+int removeElem (ABin *a, int x){
+    int r = 1;
+
+    while(*a && r){
+        if((*a)->valor == x){
+            removeRaiz(&(*a));
+            r = 0;
+        }else if((*a)->valor > x)
+            a = &((*a)->esq);
+        else
+            a = &((*a)->dir);
+    } 
+
+    return r;
+}
+
+// Questão 2
+void rodaEsquerda (ABin *a){
+    ABin b = (*a)->dir;
+    (*a)->dir = b->esq;
+    b->esq = (*a);
+    *a = b;
+}
+void rodaDireita (ABin *a){
+    ABin b = (*a)->esq;
+    (*a)->esq = b->dir;
+    b->dir = *a;
+    *a = b;
+}
+void promoveMenor (ABin *a){
+
+    if(*a && (*a)->esq){
+        promoveMenor(&((*a)->esq));
+        rodaDireita(a);
+    }
+}
+
+void promoveMaior (ABin *a){
+
+    if(*a && (*a)->dir){
+        promoveMaior(&((*a)->dir));
+        rodaEsquerda(a);
+    }
+}
+
+ABin removeMenorAlt (ABin *a){
+
+    ABin r = NULL;
+    promoveMenor(a);
+    if(*a){
+        r = *a;
+        *a = (*a)->dir;
+    }
+    return r;
+}
+
+// Questão 3
+int constroiEspinha (ABin *a){
+
+    int r = 0;
+    while(*a){
+        promoveMenor(a);
+        a = &((*a)->dir);
+        r++;
+    }
+    return r;
+
+}
+
+ABin equilibraEspinha (ABin *a, int n) {
+	return NULL;
+}
+
+void equilibra (ABin *a) {
+}
